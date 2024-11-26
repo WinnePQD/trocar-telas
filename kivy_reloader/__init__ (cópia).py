7125217b1751cf908762ae428bba65e513cc4f25
WinnePQD <109382414+WinnePQD@users.noreply.github.com>
2024-11-25 23:01:12 -0300

    Delete kivy_reloader/__init__ (cópia).py

--- kivy_reloader/__init__ (cópia).py
+++
@@ -1,7 +0,0 @@
-from beautifulapp.screens.main_screen import MainScreen
-from kivy_reloader.app import App
-
-
-class MainApp(App):
-    def build(self):
-        return MainScreen(name="Main Screen")
